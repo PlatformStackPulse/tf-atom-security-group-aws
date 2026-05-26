35b61e85693ad2587b46aa1c58f0720da5a0e33c
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:14:19 +0100

    feat: implement tf-atom-security-group-aws module

--- variables.tf
+++ variables.tf
@@ -1,7 +1,14 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+  validation {
+    condition     = length(var.vpc_id) > 0
+    error_message = "vpc_id must not be empty."
+  }
+}
+
+variable "description" {
+  description = "Description of the security group"
+  type        = string
+  default     = null
+}
